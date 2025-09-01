923315600023d28084d57475681fdc49f187a507
Hannah Norman <hnorm0629@gmail.com>
2025-08-31 23:49:27 -0700

    Add fonts

--- DogEar/ContentView.swift
+++ DogEar/ContentView.swift
@@ -9,11 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        /*
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+        }
+        .padding()
+         */
+        
+        VStack(spacing: 20) {
+            Text("D o g   E a r")
+                .font(.custom("FatFrank-Heavy", size: 32))
+
+            Text("Author · Year · Pages")
+                .font(.custom("Mono45Headline-Regular", size: 18))
+
+            Text("This is body text in Proxima Nova Regular.")
+                .font(.custom("ProximaNova-Regular", size: 16))
         }
         .padding()
     }
